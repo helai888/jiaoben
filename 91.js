bafa988c7b5f2e4fb1797cc44ab9fb970db77031
helai888 <8238777@gmail.com>
2024-11-02 11:22:44 +0800

    Create 91.js

---
+++ 91.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+https:\/\/hjll\.rmanf\.cn\/api\/app\/vid\/h5\/m3u8\/\S+\.m3u8\?token=[^&]+&c=https:\/\/haijiaode\.zgtcpt\.shop url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
+
+[mitm]
+hostname = hjll.rmanf.cn, haijiaode.zgtcpt.shop
