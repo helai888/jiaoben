13dac98c1dbd64776d737b45c90a2a119d427661
helai888 <8238777@gmail.com>
2024-11-02 11:24:29 +0800

    Update 91.js

--- 91.js
+++ 91.js
@@ -1,5 +1,10 @@
+/*
+91系列视频
+
 [rewrite_local]
+
 https:\/\/hjll\.rmanf\.cn\/api\/app\/vid\/h5\/m3u8\/\S+\.m3u8\?token=[^&]+&c=https:\/\/haijiaode\.zgtcpt\.shop url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
 
 [mitm]
-hostname = hjll.rmanf.cn, haijiaode.zgtcpt.shop
+hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip, hjll.rmanf.cn, haijiaode.zgtcpt.shop
+*/
