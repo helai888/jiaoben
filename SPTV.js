7d1097e0d8ec062b83d85ed50cc28e76f26764d0
鹤来 <8238777@gmail.com>
2024-11-04 19:48:41 +0800

    Update SPTV.js

--- SPTV.js
+++ SPTV.js
@@ -1,8 +1,10 @@
-#!name=屏蔽视频TV广告
-#!desc=屏蔽并动态替换特定视频广告
+#!name=屏蔽视频TV广告替换
+#!desc=检测广告视频并替换为自定义m3u8链接
+# 本模块检测指定广告视频链接并将其替换为自定义内容
 
-[Rewrite]
-^https:\/\/oss\.yingshi\.tv\/videos\/vod\/vi\/(.*)\.mp4 url 302 https://m3u8.hmrvideo.com/play/$1.m3u8
+[Script]
+# 使用正则表达式检测并替换广告视频链接
+^https:\/\/(?:m3u8\.hmrvideo\.com|api\.heimuer\.app)\/play\/[a-z0-9]{32}\.m3u8$ url replace https://m3u8.hmrvideo.com/play/yourcustomhash.m3u8
 
 [MITM]
-hostname = oss.yingshi.tv, m3u8.hmrvideo.com
+hostname = m3u8.hmrvideo.com, api.heimuer.app
