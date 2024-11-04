d0444a4bdf34d5b93cf0f3aa13f7e3c2efb6da9a
鹤来 <8238777@gmail.com>
2024-11-04 19:39:03 +0800

    Update SPTV.js

--- SPTV.js
+++ SPTV.js
@@ -1,8 +1,8 @@
 #!name=屏蔽视频TV广告
-#!desc=屏蔽并替换特定视频广告
+#!desc=屏蔽并动态替换特定视频广告
 
 [Rewrite]
-^https:\/\/oss\.yingshi\.tv\/videos\/vod\/vi\/.*\.mp4 url 302 https://m3u8.hmrvideo.com/play/32a93186ea8a4629b5fcb542c0881cd6.m3u8
+^https:\/\/oss\.yingshi\.tv\/videos\/vod\/vi\/(.*)\.mp4 url 302 https://m3u8.hmrvideo.com/play/$1.m3u8
 
 [MITM]
-hostname = oss.yingshi.tv
+hostname = oss.yingshi.tv, m3u8.hmrvideo.com
