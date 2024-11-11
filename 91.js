baba96c894277d955a20cb0e5afcff4ab45392b7
鹤来 <8238777@gmail.com>
2024-11-12 00:28:22 +0800

    Update 91.js

--- 91.js
+++ 91.js
@@ -3,8 +3,8 @@
 
 [rewrite_local]
 
-https:\/\/hjll\.rmanf\.cn\/api\/app\/vid\/h5\/m3u8\/\S+\.m3u8\?token=[^&]+&c=https:\/\/haijiaode\.zgtcpt\.shop url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
+https:\/\/hjll\.okkss\.xyz\/api\/app\/vid\/h5\/m3u8\/\S+\.m3u8\?token=[^&]+&c=https:\/\/haijiaode\.zgtcpt\.shop url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
 
 [mitm]
-hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip, hjll.rmanf.cn, haijiaode.zgtcpt.shop, *.example1.com, *.example2.com
+hostname = %APPEND% hjll.okkss.xyz, haijiaode.zgtcpt.shop
 */
