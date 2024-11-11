7c647749bd90a5624747ef033f4075efe9d64a6f
鹤来 <8238777@gmail.com>
2024-11-12 01:09:18 +0800

    Update 91.js

--- 91.js
+++ 91.js
@@ -3,8 +3,8 @@
 
 [rewrite_local]
 
-https:\/\/hjll\.(okkss\.xyz|rmanf\.cn)\/api\/app\/vid\/h5\/m3u8\/\S+\.m3u8\?token=[^&]+&c=https:\/\/haijiaode\.zgtcpt\.shop url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
+https:\/\/hjll\.okkss\.xyz\/api\/app\/vid\/h5\/m3u8\/\S+\.m3u8\?token=[^&]+&c=https:\/\/haijiaode\.zgtcpt\.shop url script-request-header https://raw.githubusercontent.com/helai888/jiaoben/refs/heads/main/91anwang.js
 
 [mitm]
-hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip, hjll.(rmanf\.cn|okkss\.xyz), haijiaode.zgtcpt.shop
+hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip, hjll.rmanf.cn, haijiaode.zgtcpt.shop
 */
