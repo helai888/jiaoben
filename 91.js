5f3c853ae0bb50a93bbd04b45c9fda8128ea3481
鹤来 <8238777@gmail.com>
2024-11-12 00:49:58 +0800

    Update 91.js

--- 91.js
+++ 91.js
@@ -2,9 +2,8 @@
 91系列视频
 
 [rewrite_local]
-
 https:\/\/hjll\.okkss\.xyz\/api\/app\/vid\/h5\/m3u8\/\S+\.m3u8\?token=[^&]+&c=https:\/\/haijiaode\.zgtcpt\.shop url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
 
 [mitm]
-hostname = %APPEND% hjll.okkss.xyz, haijiaode.zgtcpt.shop
+hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip, hjll.rmanf.cn, haijiaode.zgtcpt.shop
 */
