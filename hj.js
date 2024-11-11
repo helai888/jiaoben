821c6246f7b511ab65921c2a181c3c1ec84c53db
鹤来 <8238777@gmail.com>
2024-11-12 00:43:08 +0800

    Create hj.js

---
+++ hj.js
@@ -0,0 +1,10 @@
+/*
+91系列视频
+
+[rewrite_local]
+
+https:\/\/hjll\.okkss\.xyz\/api\/app\/vid\/h5\/m3u8\/\S+\.m3u8\?token=[^&]+&c=https:\/\/haijiaode\.zgtcpt\.shop url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
+
+[mitm]
+hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip, hjll.rmanf.cn, haijiaode.zgtcpt.shop, *.example1.com, *.example2.com
+*/
