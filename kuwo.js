01280e0eb63cd2d5175e6ee6e73d1f45f1313f02
鹤来 <8238777@gmail.com>
2024-11-21 17:33:15 +0800

    Update kuwo.js

--- kuwo.js
+++ kuwo.js
@@ -15,8 +15,8 @@
 http-response = true
 
 [Rule]
-USER-AGENT,KWPlayer*,PROXY
-DOMAIN-SUFFIX,kuwo.cn,PROXY
+USER-AGENT,KWPlayer*,国内媒体
+DOMAIN-SUFFIX,kuwo.cn,国内媒体
 
 [Script]
 http-response ^(?!.*img).*?kuwo\.cn(/vip|/openapi)?(/enc|/v[\d]/(user/vip\?(vers|apiVersion|platform|op\=ui|_t)|theme\?op=gd|sysinfo\?op=getRePayAndDoPayBoxNew|api(/pay)?/((user/personal/)?user/info|payInfo/kwplayer/payMiniBar|advert/(myPage|iListen|album))|album/(adBar|myRec/vipMusic)|app/newMenuList/menuListInfo|tingshu/index/radio)|/kuwopay/vip-tab/setting|/(audioApi/)?a\.p($|\?op\=getvip|.*?ptype\=vip)|/mobi\.s\?f\=kwxs|/music\.pay\?newver\=3$|/(EcomResource|(Mobile)?Ad)Serv(er|ice)) script-path=https://napi.ltd/script/Worker/KuWo.js, requires-body=true, timeout=60, tag=酷我音乐
