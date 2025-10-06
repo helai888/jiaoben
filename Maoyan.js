ae118df01e71ccdf13b5aef10659d16f09383d35
鹤来 <8238777@gmail.com>
2025-10-06 22:28:58 +0800

    Update Maoyan.js

--- Maoyan.js
+++ Maoyan.js
@@ -1,5 +1,5 @@
 [Script]
-http-response ^https?:\/\/.*\.maoyan\.com\/my\/odea\/project\/detail script-path=https://raw.githubusercontent.com/helai888/jiaoben/main/maoyan_fix_detail.js
+http-response ^https?:\/\/.*\.maoyan\.com\/my\/odea\/project\/detail script-path=https://raw.githubusercontent.com/helai888/jiaoben/refs/heads/main/maoyan_fix_detail.js
 
 [MITM]
 hostname = *.maoyan.com
