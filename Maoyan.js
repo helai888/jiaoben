2464bdb2fa1f59b03c07cf1430bdf20c97235016
鹤来 <8238777@gmail.com>
2025-10-06 22:39:47 +0800

    Update Maoyan.js

--- Maoyan.js
+++ Maoyan.js
@@ -1,5 +1,6 @@
 [Script]
-script-response-body ^https?:\/\/.*\.maoyan\.com\/my\/odea\/project\/detail script-path=https://raw.githubusercontent.com/helai888/jiaoben/main/maoyan_fix_detail.js
+# 修改 project/detail 响应体（必须放在 Script 区）
+script-response-body ^https?:\/\/.*\.maoyan\.com\/my\/odea\/project\/detail script-path=https://raw.githubusercontent.com/helai888/jiaoben/main/maoyan_fix_detail.js, requires-body=true
 
 [MITM]
 hostname = *.maoyan.com
