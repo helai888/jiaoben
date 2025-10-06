565b80244ec7efc11e9d4cbb47952872476dc32b
鹤来 <8238777@gmail.com>
2025-10-06 22:22:36 +0800

    Update maoyan_project_detail.js

--- maoyan_project_detail.js
+++ maoyan_project_detail.js
@@ -1,10 +1,20 @@
-let body = $response.body;
-if (body) {
-  body = body
-    .replace(/"stockOutRegister":1/g, '"stockOutRegister":0')
-    .replace(/"ticketStatus":2/g, '"ticketStatus":3')
-    .replace(/"saleStatus":4/g, '"saleStatus":3');
-  $done({ body });
-} else {
-  $done({});
-}
+// maoyan_fix_detail.js
+(function() {
+  var body = $response.body;
+  try {
+    var obj = JSON.parse(body);
+    if (obj && obj.data && obj.data.baseProjectVO) {
+      obj.data.baseProjectVO.stockOutRegister = 0;
+      obj.data.baseProjectVO.ticketStatus = 3;
+      obj.data.baseProjectVO.saleStatus = 3;
+    }
+    $done({
+      body: JSON.stringify(obj),
+      headers: Object.assign({}, $response.headers, {
+        'X-Maoyan-Patched': '1'
+      })
+    });
+  } catch(e) {
+    $done({body});
+  }
+})();
